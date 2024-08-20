dbe8ae92c88d723055c957818738d24cf838e7d4
sagar-sangle <ssangle263@gmail.com>
2024-08-20 19:37:32 +0530

    express ejs 2

--- Node udmy/templating/ejs/index.js
+++ Node udmy/templating/ejs/index.js
@@ -1,13 +1,14 @@
    const express = require('express')
    const app = express();
    const path = require('path')
+   const redditData = require('./data.json')
 
 
    //if u try to run index.js from other some other path ejs will not render beacuse it will
    //try to find .ejs file in that folder where u rexecuting from therefor we will use path libary 
    //in this way ejs will run from index.js 's corrpsoing path
    
-   
+   app.use(express.static('public'))
    app.set('view engine','ejs')
    app.set('views',path.join(__dirname,'/views'))
    app.use("/home",(req,res)=>{
@@ -18,4 +19,29 @@
    }).listen(8080,()=>{
     console.log("listening on port 8080");
     
+   })
+
+   app.get('/cats',(req,res)=>{
+      let cats = ['tom','gojo','chiki','baby','mushu','pillu','kitty']
+      res.render('cats',{cats})
+
+   })
+
+   app.get('/random',(req,res)=>{
+
+      let randNumber = Math.floor(Math.random()*10);
+      return res.render('random',{rand:randNumber})
+   })
+
+   app.get('/reddit/:subreddit',(req,res)=>{
+      let {subreddit} = req.params;
+
+      const Data = redditData[subreddit];
+
+      if(Data){
+         res.render('subreddit',{...Data})
+      }else{
+
+      }
+
    })
